506412a58b1cc2838f8b6dd8a266c986ab177667
Eka Putra <ekaputtra@gmail.com>
2019-02-10 21:32:00 +0700

    titik awal

---
+++ src/Form.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+    // jika menggunakan ini value di form submit tidak akan hilang setelah di klik submit
+    // state = {
+    //     name: '',
+    //     job: ''
+    // }
+
+    // ini sama perilakunya seperti di atas
+    // constructor(props) {
+    //     super(props)
+
+    //     this.state = {
+    //         name: '',
+    //         job: ''
+    //     }
+    // }
+
+    initialState = {
+        name: '',
+        job: ''
+    }
+    
+    state = this.initialState
+
+    // constructor(props) {
+    //     super(props)
+
+    //     this.initialState = {
+    //         name: '',
+    //         job: ''
+    //     }
+
+    //     this.state = this.initialState
+    // }
+
+    handleChange = event => {
+        const {name, value} = event.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+    submitForm = () => {
+        this.props.handleSubmit(this.state)
+        this.setState(this.initialState)
+    }
+
+    render() {
+        const {name, job} = this.state
+
+        return (
+            <form>
+                <label>Name</label>
+                <input 
+                    type="text"
+                    name="name"
+                    value={name}
+                    onChange={this.handleChange}/>
+                <label>Job</label>
+                <input
+                    type="text"
+                    name="job"
+                    value={job}
+                    onChange={this.handleChange}/>
+                <input
+                    type="button"
+                    value="Submit"
+                    onClick={this.submitForm}/>
+            </form>
+        )
+    }
+}
+
+export default Form
